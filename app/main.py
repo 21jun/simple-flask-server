10e276a1e9e51952d9a463c7a54a4f94de4f464e
21jun <21jun7654@gmail.com>
2019-08-09 20:29:25 +0900

    init

---
+++ app/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return 'Hello World!'
+
+@app.route('/home')
+def home_page():
+    return 'this is home'
+
+if __name__=='__main__':
+    app.run(host = '0.0.0.0', port=8000)
