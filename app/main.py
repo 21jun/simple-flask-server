6261dc0cd21ec157c1be98488971aac7608cf468
21jun <21jun7654@gmail.com>
2019-08-12 02:34:20 +0900

    change to localhost

--- app/main.py
+++ app/main.py
@@ -12,5 +12,7 @@ def main():
 def home_page():
     return 'this is home'
 
+
 if __name__=='__main__':
-    app.run(host = '0.0.0.0', port=8000)
+    # app.run(host = '0.0.0.0', port=8000)
+    app.run()
